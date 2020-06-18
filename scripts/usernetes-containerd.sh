6e8379ae3f0fc8e267f97c8aa33e8c35b6c254d2
githubfoam <lequestionis@gmail.com>
2020-06-18 12:04:34 +0300

    from there on in the film just continues getting better

--- scripts/usernetes-containerd.sh
+++ scripts/usernetes-containerd.sh
@@ -6,4 +6,13 @@ echo "=============================usernetes containerd run times ==============
 USERNETES_VERSION=20200611.1
 wget https://github.com/rootless-containers/usernetes/releases/download/v$USERNETES_VERSION/usernetes-x86_64.tbz
 tar xjvf usernetes-x86_64.tbz &&  cd usernetes
+#install.sh installs Usernetes systemd units to $HOME/.config/systemd/unit
+#use containerd as the CRI runtime (default):
+./install.sh --cri=containerd #Failed to connect to bus: Operation not permitted
+# - export KUBECONFIG="$HOME/.config/usernetes/master/admin-localhost.kubeconfig" #enable CoreDNS
+# - sudo kubectl apply -f manifests/*.yaml
+# - kubectl get nodes -o wide
+# - ./uninstall.sh
+# - ./show-cleanup-command.sh
+# - eval $(./show-cleanup-command.sh)
 echo "=============================usernetes containerd run times ============================================================="
