0eed12e041c9884ace74578d478090ba90c8e8fa
githubfoam <lequestionis@gmail.com>
2020-06-20 10:26:16 +0300

    на свой страх и риск

--- scripts/deploy-vagrant.sh
+++ scripts/deploy-vagrant.sh
@@ -16,15 +16,15 @@ export VAGRANT_CURRENT_VERSION="$(curl -s https://checkpoint-api.hashicorp.com/v
 
 # https://www.vagrantup.com/downloads.html
 # https://releases.hashicorp.com/vagrant/2.2.9/vagrant_2.2.9_SHA256SUMS
-export VAGRANT_SHA256_URL="https://releases.hashicorp.com/vagrant/$VAGRANT_CURRENT_VERSION/vagrant_${VAGRANT_CURRENT_VERSION}_SHA256SUMS"
+# export VAGRANT_SHA256_URL="https://releases.hashicorp.com/vagrant/$VAGRANT_CURRENT_VERSION/vagrant_${VAGRANT_CURRENT_VERSION}_SHA256SUMS"
 # https://releases.hashicorp.com/vagrant/2.2.9/vagrant_2.2.9_SHA256SUMS.sig
-export  VAGRANT_SHA256_SIG_URL="https://releases.hashicorp.com/vagrant/$VAGRANT_CURRENT_VERSION/vagrant_${VAGRANT_CURRENT_VERSION}_SHA256SUMS.sig"
+# export  VAGRANT_SHA256_SIG_URL="https://releases.hashicorp.com/vagrant/$VAGRANT_CURRENT_VERSION/vagrant_${VAGRANT_CURRENT_VERSION}_SHA256SUMS.sig"
 # curl -0(zero) [url of remote file] -o(small O) output-file.
 # curl -LO "${VAGRANT_URL}"
 # curl -LO "${VAGRANT_SHA256_URL}"
 # curl -LO "${VAGRANT_SHA256_SIG_URL}"
-export HASHICORP_PUBLIC_KEY_URL="https://keybase.io/hashicorp/pgp_keys.asc" #https://www.hashicorp.com/security
-export 'curl -sSL "${HASHICORP_PUBLIC_KEY_URL}" | gpg --import -' # import the public key (PGP key)
+# export HASHICORP_PUBLIC_KEY_URL="https://keybase.io/hashicorp/pgp_keys.asc" #https://www.hashicorp.com/security
+# export 'curl -sSL "${HASHICORP_PUBLIC_KEY_URL}" | gpg --import -' # import the public key (PGP key)
 # gpg --verify "vagrant_${VAGRANT_CURRENT_VERSION}_SHA256SUMS.sig" "vagrant_${VAGRANT_CURRENT_VERSION}_SHA256SUMS" 2>/dev/null #Verify the signature file is untampered
 # shasum -a 256 -c "vagrant_${VAGRANT_CURRENT_VERSION}_SHA256SUMS" | sudo tee output.txt  # Verify the SHASUM matches the archive.
 # cat output.txt  | grep OK # print OK
